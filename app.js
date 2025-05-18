a9b2041dcc55efcdaffbcbbf50a898f2d01372ad
khaulanauman <khaulanauman@gmail.com>
2025-05-18 19:42:43 +0500

    fixed

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var connection = require('./app/config/connection');
-var routes = require('./app/controllers/routes');
+const app = express();
+const connection = require('./app/config/connection');
+const routes = require('./app/controllers/routes');
 
-var app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+// Serve frontend files
+app.use(express.static('public'));
+
+// Middleware for parsing form and JSON data
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Init DB and routes
 connection.init();
 routes.configure(app);
 
-var server = app.listen(8000, function(){
-  console.log('Server listening on port ' + server.address().port);
+// Start server
+const server = app.listen(8000, () => {
+  console.log('✅ Server listening on port ' + server.address().port);
 });
